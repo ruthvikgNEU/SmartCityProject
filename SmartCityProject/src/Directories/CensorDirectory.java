4b03e2ebbf86c7d6e00521783219f569223cee97
ruthvikgNEU <garlapati.r@northeastern.edu>
2022-12-08 20:43:30 -0500

    mcreator code added

--- SmartCityProject/src/Directories/CensorDirectory.java
+++ SmartCityProject/src/Directories/CensorDirectory.java
@@ -35,4 +35,24 @@ public class CensorDirectory {
         }
         return null;
     }
+    
+    
+    public void insertRecord(String name,String dir,String prod,String url,String theatre,String applied_Date){
+        try{
+        PreparedStatement st= (PreparedStatement)connection.prepareStatement("insert into censor_applications(theatre,movie_name,director,studio,app_status,movie_status,applied_date,url) values (?,?,?,?,?,?,?,?)");
+        st.setString(1,theatre);
+        st.setString(2,name);
+        st.setString(3, dir);
+        st.setString(4,prod);
+        st.setString(5, "Active");
+        st.setString(6, "Pending");
+        st.setString(7, applied_Date);
+        st.setString(8, url);
+        st.executeUpdate();
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
+       
+    }
 }
